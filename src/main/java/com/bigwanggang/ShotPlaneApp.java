523232ded030fc903135f79f1ba07ab987c23e29
bigwanggang <bigwanggang@163.com>
2017-10-25 20:01:14 +0800

    创建工程

---
+++ src/main/java/com/bigwanggang/ShotPlaneApp.java
@@ -0,0 +1,13 @@
+package com.bigwanggang;
+
+/**
+ * Hello world!
+ *
+ */
+public class ShotPlaneApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
