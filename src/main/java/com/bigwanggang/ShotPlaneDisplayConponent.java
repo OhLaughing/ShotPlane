85a7194b76d794cfa6414a6944844eb2446a28bc
bigwanggang <bigwanggang@163.com>
2017-10-28 17:19:59 +0800

    Update ShotPlaneDisplayConponent.java

--- src/main/java/com/bigwanggang/ShotPlaneDisplayConponent.java
+++ src/main/java/com/bigwanggang/ShotPlaneDisplayConponent.java
@@ -2,51 +2,86 @@ package com.bigwanggang;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-/**
- * Created by gustaov on 2017/10/25.
- */
 public class ShotPlaneDisplayConponent extends JComponent {
-    private static final int DEFAULT_WIDTH = 400;
-    private static final int DEFAULT_HEIGHT = 300;
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 290;
+    private List<Line2D> line2DList = null;
+    private Map<Rectangle2D, Color> squares;
+    Graphics2D g2;
+    private Plane plane;
 
-    public void paintComponent(Graphics g)
-    {
-        Graphics2D g2 = (Graphics2D) g;
-
-        // draw a rectangle
-
-        double leftX = 100;
-        double topY = 100;
-        double width = 200;
-        double height = 150;
-
-        Rectangle2D rect = new Rectangle2D.Double(leftX, topY, width, height);
-        g2.draw(rect);
-
-        // draw the enclosed ellipse
-
-        Ellipse2D ellipse = new Ellipse2D.Double();
-        ellipse.setFrame(rect);
-        g2.draw(ellipse);
-
-        // draw a diagonal line
-
-        g2.draw(new Line2D.Double(leftX, topY, leftX + width, topY + height));
-
-        // draw a circle with the same center
-
-        double centerX = rect.getCenterX();
-        double centerY = rect.getCenterY();
-        double radius = 150;
-
-        Ellipse2D circle = new Ellipse2D.Double();
-        circle.setFrameFromCenter(centerX, centerY, centerX + radius, centerY + radius);
-        g2.draw(circle);
+    public ShotPlaneDisplayConponent(Plane plane) {
+        this.plane = plane;
+        line2DList = new ArrayList<>();
+        squares = new HashMap<>();
+        for (int i = 0; i < 15; i++) {
+            line2DList.add(new Line2D.Double(10, 10 + i * 20, 290, 10 + i * 20));
+            line2DList.add(new Line2D.Double(10 + i * 20, 10, 10 + i * 20, 290));
+        }
+        for (int i = 0; i < 14; i++) {
+            for (int j = 0; j < 14; j++) {
+                Rectangle2D rectangle2D = new Rectangle(10 + i * 20, 10 + j * 20, 20, 20);
+                squares.put(rectangle2D, new Color(54, 63, 61));
+            }
+        }
+        addMouseListener(new ShotPlaneHandler());
     }
 
-    public Dimension getPreferredSize() { return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT); }
+    public void paintComponent(Graphics g) {
+        g2 = (Graphics2D) g;
+
+        for (Line2D line : line2DList)
+            g2.draw(line);
+
+        for (Rectangle2D rectangle2D : squares.keySet()) {
+            g2.setPaint(squares.get(rectangle2D));
+            g2.fill(rectangle2D);
+            g2.setPaint(Color.BLACK);
+            g2.draw(rectangle2D);
+        }
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    private class ShotPlaneHandler extends MouseAdapter {
+        public void mouseClicked(MouseEvent event) {
+            Point point = event.getPoint();
+            if (event.getClickCount() >= 2) {
+                int x = (int) point.getX();
+                int y = (int) point.getY();
+                if (x > 10 && x < 290 && y > 10 && y < 290) {
+                    int xN = (x - 10) / 20;
+                    int yN = (y - 10) / 20;
+                    System.out.println(xN + "," + yN);
+                    Line2D line2D = new Line2D.Double(xN * 20 + 10, yN * 20 + 10, xN * 20 + 30, yN * 20 + 30);
+                    Rectangle2D rectangle2D = new Rectangle2D.Double(xN * 20 + 10, yN * 20 + 10, 20, 20);
+                    Point p = new Point(xN, yN);
+                    if (Util.ifHitDownPlane(plane, p)) {
+                        squares.put(rectangle2D, Color.BLUE);
+                        System.out.println("hit down the plane");
+                    }
+                    else if (Util.ifHitPlane(plane, p)) {
+                        squares.put(rectangle2D, Color.BLUE);
+                        System.out.println("hit the plane, but does not hit down");
+                    } else {
+                        squares.put(rectangle2D, Color.WHITE);
+                        System.out.println("does not hit plane");
+                    }
+                    line2DList.add(line2D);
+                    repaint();
+                }
+            }
+        }
+    }
 }
