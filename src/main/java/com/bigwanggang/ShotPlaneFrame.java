c7516eed5afc8136ccd78a2255ac9cf43d9be6e0
bigwanggang <bigwanggang@163.com>
2017-10-31 17:52:35 +0800

    Update ShotPlaneFrame.java

--- src/main/java/com/bigwanggang/ShotPlaneFrame.java
+++ src/main/java/com/bigwanggang/ShotPlaneFrame.java
@@ -7,37 +7,94 @@ import java.awt.event.ActionListener;
 import java.awt.geom.Rectangle2D;
 
 public class ShotPlaneFrame extends JFrame {
-    private JPanel controlPanel;
-    private JPanel displayPanel;
-    private JPanel playPanel;
-    private static final int DEFAULT_WIDTH = 500;
-    private static final int DEFAULT_HEIGHT = 400;
+    private static final int DEFAULT_WIDTH = 800;
+    private static final int DEFAULT_HEIGHT = 350;
     private Plane plane = new Plane(6, 5, 6, 8);
-    private ShotPlaneDisplayConponent displayComponent;
+    private ShotPlaneDisplayConponent gameDisplayComponent;
+    private JPanel controlPanel;
     private JTextField ipField;
     private JTextField portField;
     private JTextArea chatDisplayArea;
     private JTextArea chatInputArea;
+    private GridBagConstraints constraints;
 
     public ShotPlaneFrame() {
-        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        displayPanel = new JPanel();
-        playPanel = new JPanel();
-        displayComponent = new ShotPlaneDisplayConponent(plane);
-
-        displayPanel.add(displayComponent);
-
         controlPanel = new JPanel();
-        controlPanelInit(controlPanel);
-        add(displayPanel, BorderLayout.CENTER);
-        add(controlPanel, BorderLayout.EAST);
+        chatDisplayArea = new JTextArea(6, 6);
+        chatInputArea = new JTextArea(6, 3);
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        gameDisplayComponent = new ShotPlaneDisplayConponent(plane);
+        constraints = new GridBagConstraints();
+        ipField = new JTextField(10);
+        portField = new JTextField(3);
+
+        //游戏显示和信息显示的分隔符
+        JSplitPane pane1 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        pane1.setDividerSize(1);
+        pane1.setDividerLocation(300);
+        pane1.setEnabled(false);
+        getContentPane().add(pane1, BorderLayout.CENTER);
+
+        //信息显示和控制栏的分隔符
+        JSplitPane dis_control_split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        dis_control_split.setDividerLocation(200);
+        dis_control_split.setEnabled(false);
+        //分隔符左侧是游戏显示窗口
+        pane1.setLeftComponent(gameDisplayComponent);
+        pane1.setRightComponent(dis_control_split);
+
+        //信息显示和信息输入的分隔符
+        JSplitPane dis_input_split = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+        dis_input_split.setDividerLocation(220);
+        dis_control_split.setLeftComponent(dis_input_split);
+        dis_control_split.setRightComponent(controlPanel);
+        dis_input_split.setLeftComponent(chatDisplayArea);
+
+        //信息输入和发送信息按钮分隔
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        split.setDividerLocation(150);
+        dis_input_split.setRightComponent(split);
+        split.setLeftComponent(chatInputArea);
+        JButton sendButton = new JButton("SEND");
+        split.setRightComponent(sendButton);
+
+        controlPanelInit();
 //        setResizable(false);
     }
 
-    private void controlPanelInit(JPanel controlPanel) {
-        controlPanel.setLayout(new GridLayout(8, 1));
-        JPanel movePanel = new JPanel();
-        JPanel rotatePanel = new JPanel();
+    private void controlPanelInit() {
+        controlPanel.setLayout(new GridLayout(2, 1));
+
+        initIpPortInputPanel();
+
+        initDirectPanel();
+        setResizable(false);
+    }
+
+    private void initIpPortInputPanel() {
+        GridBagLayout layout = new GridBagLayout();
+
+        JPanel ipPortPanel = new JPanel();
+        ipPortPanel.setLayout(layout);
+
+        JLabel ipLabel = new JLabel("IP:");
+        JLabel portLabel = new JLabel("Port:");
+        JButton connectButton = new JButton("connect");
+        connectButton.addActionListener(new ConnectAction(ipField, portField));
+
+
+        constraints.anchor = GridBagConstraints.SOUTH;
+        add(ipPortPanel, ipLabel, constraints, 0, 0, 1, 1);
+        add(ipPortPanel, ipField, constraints, 1, 0, 5, 1);
+        add(ipPortPanel, portLabel, constraints, 0, 1, 1, 1);
+        add(ipPortPanel, portField, constraints, 1, 1, 3, 1);
+        add(ipPortPanel, connectButton, constraints, 8, 0, 1, 2);
+
+        controlPanel.add(ipPortPanel);
+    }
+
+    private void initDirectPanel() {
+        JPanel directionPanel = new JPanel();
         JButton leftButton = new JButton("Left");
         JButton upButton = new JButton("Up");
         JButton downButton = new JButton("Down");
@@ -50,66 +107,32 @@ public class ShotPlaneFrame extends JFrame {
         rightButton.addActionListener(moveListener);
         okButton.addActionListener(moveListener);
 
-        movePanel.add(leftButton);
-        movePanel.add(okButton);
-        movePanel.add(rightButton);
-
-        JPanel upPanel = new JPanel();
-        upPanel.add(upButton);
-        controlPanel.add(upPanel);
-        controlPanel.add(movePanel);
-        JPanel downPanel = new JPanel();
-        downPanel.add(downButton);
-        controlPanel.add(downPanel);
-//        rotatePanel.setLayout(new GridLayout(1, 2));
-
         JButton rotate1 = new JButton("<<");
         JButton rotate2 = new JButton(">>");
         rotate1.addActionListener(moveListener);
         rotate2.addActionListener(moveListener);
-        rotatePanel.add(rotate1);
-        rotatePanel.add(rotate2);
 
-        controlPanel.add(rotatePanel);
-        ipField = new JTextField(10);
-//        ipPoint.setEnabled(false);
-        portField = new JTextField(3);
-        JPanel ipPanel = new JPanel();
         GridBagLayout layout = new GridBagLayout();
-        ipPanel.setLayout(layout);
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.weightx = 10;
-        constraints.weighty = 10;
-        constraints.gridx = 0;
-        constraints.gridy = 2;
-        constraints.gridwidth = 2;
-        constraints.gridheight = 1;
-        JLabel ipLabel = new JLabel("IP:", SwingConstants.RIGHT);
-        ipPanel.add(ipLabel, constraints);
-        JLabel portLabel = new JLabel("Port:", SwingConstants.RIGHT);
-        ipPanel.add(portLabel, constraints);
-        ipPanel.add(ipLabel);
-        ipPanel.add(ipField);
-        ipPanel.add(portLabel);
-        ipPanel.add(portField);
 
-        //connect button
-        JPanel connectPanel = new JPanel();
-        JButton connectButton = new JButton("connect");
-        connectButton.addActionListener(new ConnectAction(ipField, portField));
+        directionPanel.setLayout(layout);
+        constraints.anchor = GridBagConstraints.NORTH;
+        add(directionPanel, leftButton, constraints, 0, 1, 1, 1);
+        add(directionPanel, upButton, constraints, 1, 0, 1, 1);
+        add(directionPanel, downButton, constraints, 1, 2, 1, 1);
+        add(directionPanel, rightButton, constraints, 2, 1, 1, 1);
+        add(directionPanel, okButton, constraints, 1, 1, 1, 1);
 
-        connectPanel.add(connectButton);
-        controlPanel.add(ipPanel);
-        controlPanel.add(connectPanel);
+        add(directionPanel, rotate1, constraints, 4, 0, 1, 1);
+        add(directionPanel, rotate2, constraints, 4, 2, 1, 1);
+        controlPanel.add(directionPanel);
+    }
 
-        //chat panel
-        JPanel chatPanel = new JPanel();
-        chatDisplayArea = new JTextArea(10, 6);
-        chatInputArea = new JTextArea(8, 3);
-        chatPanel.add(chatDisplayArea);
-        chatPanel.add(chatInputArea);
-        connectButton.add(chatPanel);
-
+    public void add(JPanel panel, Component c, GridBagConstraints constraints, int x, int y, int w, int h) {//此方法用来添加控件到容器中
+        constraints.gridx = x;
+        constraints.gridy = y;
+        constraints.gridwidth = w;
+        constraints.gridheight = h;
+        panel.add(c, constraints);
     }
 
     private class ButtonAction implements ActionListener {
@@ -123,8 +146,8 @@ public class ShotPlaneFrame extends JFrame {
                 boolean e2 = head.getY() == tail.getY() && head.getX() > 0 && tail.getX() > 0;
                 if (e1 | e2) {
                     plane = new Plane(head.getX() - 1, head.getY(), tail.getX() - 1, tail.getY());
-                    displayComponent.setPlane(plane);
-                    displayComponent.repaint();
+                    gameDisplayComponent.setPlane(plane);
+                    gameDisplayComponent.repaint();
                 }
             }
             if ("Right".equals(input)) {
@@ -134,8 +157,8 @@ public class ShotPlaneFrame extends JFrame {
                 boolean e2 = head.getY() == tail.getY() && head.getX() < 13 && tail.getX() < 13;
                 if (e1 | e2) {
                     plane = new Plane(head.getX() + 1, head.getY(), tail.getX() + 1, tail.getY());
-                    displayComponent.setPlane(plane);
-                    displayComponent.repaint();
+                    gameDisplayComponent.setPlane(plane);
+                    gameDisplayComponent.repaint();
                 }
             }
             if ("Up".equals(input)) {
@@ -145,8 +168,8 @@ public class ShotPlaneFrame extends JFrame {
                 boolean e2 = head.getY() == tail.getY() && head.getY() > 2;
                 if (e1 | e2) {
                     plane = new Plane(head.getX(), head.getY() - 1, tail.getX(), tail.getY() - 1);
-                    displayComponent.setPlane(plane);
-                    displayComponent.repaint();
+                    gameDisplayComponent.setPlane(plane);
+                    gameDisplayComponent.repaint();
                 }
             }
             if ("Down".equals(input)) {
@@ -156,8 +179,8 @@ public class ShotPlaneFrame extends JFrame {
                 boolean e2 = head.getY() == tail.getY() && head.getY() < 11;
                 if (e1 | e2) {
                     plane = new Plane(head.getX(), head.getY() + 1, tail.getX(), tail.getY() + 1);
-                    displayComponent.setPlane(plane);
-                    displayComponent.repaint();
+                    gameDisplayComponent.setPlane(plane);
+                    gameDisplayComponent.repaint();
                 }
             }
             if ("<<".equals(input)) {
@@ -189,8 +212,8 @@ public class ShotPlaneFrame extends JFrame {
                     if (x == 1) x++;
                     plane = new Plane(x, y1, x, y2);
                 }
-                displayComponent.setPlane(plane);
-                displayComponent.repaint();
+                gameDisplayComponent.setPlane(plane);
+                gameDisplayComponent.repaint();
             }
             if (">>".equals(input)) {
                 Point head = plane.getHead();
@@ -220,18 +243,18 @@ public class ShotPlaneFrame extends JFrame {
                     if (x == 12) x--;
                     plane = new Plane(x, y1, x, y2);
                 }
-                displayComponent.setPlane(plane);
-                displayComponent.repaint();
+                gameDisplayComponent.setPlane(plane);
+                gameDisplayComponent.repaint();
             }
             if ("OK".equals(input)) {
                 for (int i = 0; i < 14; i++) {
                     for (int j = 0; j < 14; j++) {
                         Rectangle2D rectangle2D = new Rectangle(10 + i * 20, 10 + j * 20, 20, 20);
-                        displayComponent.addSquare(rectangle2D, new Color(54, 63, 61));
+                        gameDisplayComponent.addSquare(rectangle2D, new Color(54, 63, 61));
                     }
                 }
-                displayComponent.disablePlane();
-                displayComponent.repaint();
+                gameDisplayComponent.disablePlane();
+                gameDisplayComponent.repaint();
             }
         }
     }
